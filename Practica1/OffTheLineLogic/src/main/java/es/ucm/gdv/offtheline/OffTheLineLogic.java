b8263ab66f1a71602404bd438b693b884769e8f7
AlansonUCM <alanson@ucm.es>
2020-11-26 19:54:21 +0100

    Empezada carga de archivo

--- Practica1/OffTheLineLogic/src/main/java/es/ucm/gdv/offtheline/OffTheLineLogic.java
+++ Practica1/OffTheLineLogic/src/main/java/es/ucm/gdv/offtheline/OffTheLineLogic.java
@@ -1,11 +1,66 @@
 package es.ucm.gdv.offtheline;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONAware;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import es.ucm.gdv.engine.Engine;
 import es.ucm.gdv.engine.Game;
+
 
 /**
  * Implementa la interfaz lógica del juego.
  */
 public class OffTheLineLogic implements Game {
+
+    Engine engine=null;
+    JSONArray levels=null;
+    public OffTheLineLogic(Engine engine)
+    {
+        this.engine=engine;
+
+        InputStream is=engine.openInputStream("Resources/levels.json");
+        InputStreamReader isr=new InputStreamReader(is);
+        JSONParser jsonParser=new JSONParser();
+
+        try {
+            levels =(JSONArray) jsonParser.parse(isr);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+       System.out.print(((JSONObject)levels.get(0)).toJSONString());
+    }
+
+    public GameObject[] loadLevel(int i)
+    {
+        GameObject[] objects;
+
+        if(levels!=null) {
+
+            if(i<levels.size())
+            {
+
+
+                JSONObject level=(JSONObject)levels.get(0);
+                JSONArray walls=(JSONArray)level.get("paths");
+                for(int j=0;j< walls.size();j++)
+                {
+                   JSONArray vertices= (JSONArray)((JSONObject)walls.get(j)).get("vertices");
+                   //////////////////////////////////// Coger los vertices y crear el objeto ahora
+                }
+            }
+        }
+        return null;
+    }
 
     public void update(float deltaTime){
 
--- Practica1/desktopGame/src/main/java/es/ucm/gdv/offtheline/Main.java
+++ Practica1/desktopGame/src/main/java/es/ucm/gdv/offtheline/Main.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.util.List;
 
 
+import es.ucm.gdv.engine.desktop.Engine;
 import es.ucm.gdv.engine.desktop.Graphics;
 import es.ucm.gdv.engine.desktop.Input;
 
@@ -14,90 +15,16 @@ import es.ucm.gdv.engine.desktop.Input;
 public class Main {
     public static void main (String[] args)
     {
-        
-
-
-       // g.update();
-        System.out.println("Hola buenas");
-
-        float[] c=new float[3];
-        c[0]=0.9f;
-        c[1]=0.2f;
-        c[2]=0.2f;
-        g.clear(c);
-        float angle=0;
-        float scale=1;
-        //g.scale(1.0f,1.0f);
-        //g.save();
-
-        while(true) {
-            angle+=(float)Math.PI/10000;
-            //if(scale<10f)
-               // scale+=1f/100f;
-            c[0]=0.9f;
-            c[1]=0.2f;
-            c[2]=0.2f;
-            g.clear(c);
-
-            c[0]=0.2f;
-            c[1]=0.2f;
-            c[2]=0.9f;
-
-
-
-            g.setColor(c);
-            //g.scale(1.0f,1.0f);
-
-           // g.scale(6.0f,1.0f);
-            g.rotate(angle);
-            g.translate(400,100);
-           // g.save();
-            g.scale(scale,scale);
-
-
-
-            g.setColor(c);
-            g.drawLine(-50,-50,-50,50);
-            //g.rotate(angle);
-
-            //g.setColor(c);
-           // g.translate(-100,100);
-
-
-            g.drawLine(-50,50,50,50);
-           // g.rotate(angle);
-
-           // g.setColor(c);
-            //g.translate(400,100);
-            g.drawLine(50,50,50,-50);
-
-
-            //g.setColor(c);
-           // g.translate(400,100);
-           // g.rotate(angle);
-            g.drawLine(50,-50,-50,-50);
-
-            g.restore();
-            g.translate(100,400);
-            g.scale(4.0f,4.0f);
-            g.setColor(c);
-
-            g.rotate(0);
-            g.fillRect(-10,-10,10,10);
-
-            g.restore();
-            g.update();
-
-            List<es.ucm.gdv.engine.Input.TouchEvent> ev= i.getTouchEvents();
-            for (es.ucm.gdv.engine.Input.TouchEvent e:ev)
-            {
-                System.out.println(e.type);
-
-            }
-            ev.clear();
-
+        Engine engine=new Engine();
+        OffTheLineLogic logic=new OffTheLineLogic(engine);
+        engine.setGame(logic);
+        while(true)
+        {
 
         }
 
-    }
+       // g.update();
+
+     }
+
 }
