398968c983a944f72956f713b7f588e30a27231e
AlansonUCM <Alanson@ucm.es>
2021-01-11 13:16:34 +0100

    Colocacion del nivel
    Aun no esta hecho el escalado y posicionado correctos

--- Practica2/MazesAndMore/Assets/Src/BoardManager.cs
+++ Practica2/MazesAndMore/Assets/Src/BoardManager.cs
@@ -30,17 +30,44 @@ namespace MazesAndMore
 
         public void SetMap(Map map)
         {
-            tiles = new Tile[map.GetHeight(), map.GetWidth()];
+            tiles = new Tile[map.GetHeight()+1, map.GetWidth()+1];
+            List<Point> mapIce;
+            mapIce = map.GetIceTiles();
+            List<Wall> mapWalls;
+            mapWalls = map.GetWalls();
             // Instancia las tiles del nivel
-            for (int r = 0; r < map.GetHeight(); r++)
+            for (int r = 0; r < map.GetHeight()+1; r++)
             {
-                for (int c = 0; c < map.GetWidth(); c++)
+                for (int c = 0; c < map.GetWidth()+1; c++)
                 {
                     Tile tile = CreateTile(c + xOffset, r + yOffset);
                     tiles[r, c] = tile;
-
+                    tile.setColor(Color.white);
                     // Propiedades del tile
-                    if (map.GetGoal().x == c && map.GetGoal().y == r) tile.EnableGoal();
+                    if (map.GetGoal().x+1 == c && map.GetGoal().y+1 == r) tile.EnableGoal();
+                   
+                   
+
+                    if (c == 0 && r!=0) tile.EnableRightWall();
+
+                    if (r == 0 && c!=0) tile.EnableTopWall();
+
+                    Point pos; pos.x = c-0.5f; pos.y = r-0.5f;
+                    if (mapIce.Contains(pos)) 
+                        tile.EnableIce();
+
+
+                    Point originX; originX.x = c-1; originX.y = r;
+                    Point destiny; destiny.x = c; destiny.y = r;
+                    Wall helpX; helpX.o = originX; helpX.d = destiny;
+
+                    if(mapWalls.Contains(helpX)) tile.EnableTopWall();
+
+                    Point originY; originY.x = c ; originY.y = r-1;
+                    Wall helpY; helpY.o = destiny; helpY.d = originY;
+
+                    if (mapWalls.Contains(helpY)) tile.EnableRightWall();
+                    
                 }
             }
 
