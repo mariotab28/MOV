b56a55efa309d914e2672b66df6e300784a3387b
mariotab28 <38128815+mariotab28@users.noreply.github.com>
2021-01-12 02:05:14 +0100

    Empezado Input del jugador

---
+++ Practica2/MazesAndMore/Assets/Src/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    // Desplaza al jugador hasta la siguiente intersección a la IZQUIERDA si es posible
+    public void MoveLeft()
+    {
+
+    }
+
+    // Desplaza al jugador hasta la siguiente intersección a la DERECHA si es posible
+    public void MoveRight()
+    {
+
+    }
+
+    // Desplaza al jugador hasta la siguiente intersección hacia ARRIBA si es posible
+    public void MoveUp()
+    {
+
+    }
+
+    // Desplaza al jugador hasta la siguiente intersección hacia ABAJO si es posible
+    public void MoveDown()
+    {
+
+    }
+}
